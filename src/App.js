a0b0d7a72ae00b8d9c15c29e3ddf974e333b1a94
Illya Chukhalo <chipilla88@gmail.com>
2023-12-06 17:50:49 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react"
+import Content from "../src/components/Content.js"
+import Footer from "../src/components/Footer.js"
+import Header from "./components/Header" 
+import SideBar from "../src/components/SideBar.js"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    
+     <Header title="Header"/>
+     <div style={{display: "flex", flexDirection:"row", width:"100%", height:"500px"}}>
+        <SideBar title="SideBar"/>
+        <Content title="Content"/>
+     </div>
+     <Footer title="Footer"/>
+     
+    </>
+
   );
 }
 
